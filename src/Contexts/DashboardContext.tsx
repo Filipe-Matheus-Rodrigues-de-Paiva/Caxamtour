a59d0b2013f231a63b3b12f40ef8f12752c61290
matheussASR <rm551466@fiap.com.br>
2023-05-04 12:51:47 -0300

    feat: filtro dashboard

--- src/Contexts/DashboardContext.tsx
+++ src/Contexts/DashboardContext.tsx
@@ -1,28 +1,87 @@
 /* Contexto para funções e estados da dashboard */
 import { createContext, useState } from "react";
+import { api } from "../Services/api";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface IChildren {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 interface IDashboardContext {
-    modal: boolean
-    setModal: React.Dispatch<React.SetStateAction<boolean>>
-    selectedFilter: string
-    setSelectedFilter: any
+  modal: boolean;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedFilter: string;
+  setSelectedFilter: any;
+  hotelsData: IGetHotels[];
+  restaurantsData: IGetRestaurants[];
+  eventsData: IGetEvents[];
+  getData: any;
+  setSelectedList: any;
+  selectedList: IGetHotels[] | IGetRestaurants[] | IGetEvents[];
 }
 
-export const DashBoardContext = createContext({} as IDashboardContext)
-
-export const DashBoardProvider = ({children}: IChildren) => {
-
-    const [modal, setModal] = useState(false)
-    const [selectedFilter, setSelectedFilter] = useState("Feed")
-
-
-    return (
-        <DashBoardContext.Provider value={{modal, setModal, selectedFilter, setSelectedFilter}}>
-            {children}
-        </DashBoardContext.Provider>
-    )
+interface IGetHotels {
+  name: string;
+  address: string;
+  description: string;
+  rooms_types: [];
+  price: number;
+  img: string;
+  id: number;
 }
+
+interface IGetRestaurants {
+  name: string;
+  address: string;
+  description: string;
+  img: string;
+  id: number;
+}
+
+interface IGetEvents {
+    name: string;
+    description: string;
+    img: string;
+    id: number;
+}
+
+export const DashBoardContext = createContext({} as IDashboardContext);
+
+export const DashBoardProvider = ({ children }: IChildren) => {
+  const [modal, setModal] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState("Feed");
+  const [selectedList, setSelectedList] = useState([])
+  const [hotelsData, setHotelsData] = useState<IGetHotels[]>([]);
+  const [restaurantsData, setRestaurantsData] = useState<IGetRestaurants[]>([]);
+  const [eventsData, setEventsData] = useState<IGetEvents[]>([]);
+
+  const getData = async () => {
+    try {
+      const responseHotels = await api.get<IGetHotels[]>("/hotels");
+      setHotelsData(responseHotels.data)
+    } catch (error: any) {
+        toast.error(error.response.data)
+    }
+    try {
+      const responseRestaurants = await api.get<IGetRestaurants[]>("/restaurants");
+      setRestaurantsData(responseRestaurants.data)
+    } catch (error: any) {
+        toast.error(error.response.data)
+    }
+    try {
+      const responseEvents = await api.get<IGetEvents[]>("/events");
+      setEventsData(responseEvents.data)
+    } catch (error: any) {
+        toast.error(error.response.data)
+    }
+  };
+
+  return (
+    <DashBoardContext.Provider
+      value={{ modal, setModal, selectedFilter, setSelectedFilter, hotelsData, restaurantsData, eventsData, getData, selectedList, setSelectedList }}
+    >
+      {children}
+    </DashBoardContext.Provider>
+  );
+};
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,12 +1,27 @@
+import { useContext, useEffect } from "react";
 import Header from "../Components/HeaderDashboard/Header";
 import Menu from "../Components/MenuNavigate/Menu";
+import { DashBoardContext } from "../Contexts/DashboardContext";
+import { CardsList } from "../Components/CardsList/CardsList";
 
 export default function Dashboard () {
+    const { getData, eventsData, restaurantsData, hotelsData } = useContext(DashBoardContext)
+
+    useEffect(() => {
+        const test = async () => {
+            await getData()
+            console.log(eventsData)
+            console.log(hotelsData)
+            console.log(restaurantsData)
+        }
+        test()
+    }, [])
 
     return (
         <>
             <Header />
             <Menu />
+            <CardsList />
         </>
     )
 }
