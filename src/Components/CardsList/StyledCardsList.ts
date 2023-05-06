79df6df85efac510a13806f7fd59fca62220a9ad
filipe-matheus <filipepaivinha@gmail.com>
2023-05-06 17:05:16 -0300

    add new background to cards and new border

--- src/Components/CardsList/StyledCardsList.ts
+++ src/Components/CardsList/StyledCardsList.ts
@@ -12,12 +12,17 @@ export const CardsListStyled = styled.section`
     gap: 30px;
     margin-bottom: 60px;
     li {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
       width: 300px;
+      border-radius: 8px;
       @media (min-width: 321px) {
         width: 31%;
       }
       height: 300px;
-      background: #d9d9d9;
+      background-color: #9DB3BE;
+      border: 2px solid #02C5FC;
       text-align: center;
       p {
         font-family: "Inter";
@@ -29,9 +34,12 @@ export const CardsListStyled = styled.section`
       }
       img {
         width: 90%;
+        align-self: center;
         height: 180px;
       }
       button {
+        align-self: center;
+        border-radius: 8px;
         border-style: none;
         background-color: #d9d9d9;
         font-family: "Inter";
@@ -40,6 +48,11 @@ export const CardsListStyled = styled.section`
         font-size: 18px;
         color: #000000;
         margin-top: 10px;
+        padding: 10px;
+
+        :hover {
+          background-color: #FFFFFF;
+        }
       }
     }
   }
